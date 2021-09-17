3672cbd216e16381892f88e190a0d2b0da2aa5a3
IvanTurashov <ivan@solarlabs.ee>
2021-09-17 17:02:43 +0200

    added appletDir as option for a constructor

--- src/lib/sudoer.js
+++ src/lib/sudoer.js
@@ -107,6 +107,8 @@ class SudoerDarwin extends SudoerUnix {
         } else if (options.icns && options.icns.trim().length === 0) {
             throw new Error('options.icns must be a non-empty string if provided.');
         }
+
+        this.appletDir = options.appletDir || `${__dirname}/bin`
         this.up = false;
     }
 
@@ -185,7 +187,7 @@ class SudoerDarwin extends SudoerUnix {
             let icon = await self.readIcns(),
                 hash = self.hash(icon);
             // Copy applet to temporary directory
-            let source = join(`${__static}/bin`, 'applet.app'),
+            let source = join(this.appletDir, 'applet.app'),
                 target = join(self.tmpdir, hash, `${self.options.name}.app`);
             try {
                 await mkdir(dirname(target));
